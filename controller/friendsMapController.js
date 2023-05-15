55b7a8578567eb2700b11b42b5a35ad9d73c3a1c
Olingo <195367@ecam.be>
2023-05-16 01:09:03 +0200

    fixing params to int

--- controller/friendsMapController.js
+++ controller/friendsMapController.js
@@ -86,9 +86,9 @@ db.sequelize.query(`SELECT * FROM umad.users WHERE iduser IN (SELECT iduser FROM
 
 
 exports.acceptFriend = function (req, res) {
-    db.FriendsMap.update({ status: "accepted" }, { where: { iduser: req.body.idfriend , idfriend:req.params.userId } }).then(function (result) {
+    db.FriendsMap.update({ status: "accepted" }, { where: { iduser: req.body.idfriend , idfriend:+req.params.userId } }).then(function (result) {
         if (result == 1) {
-            db.FriendsMap.findOne({ where: { iduser: req.params.userId, idfriend: req.body.idfriend } }).then(function (friend) {
+            db.FriendsMap.findOne({ where: { iduser:+ req.params.userId, idfriend: req.body.idfriend } }).then(function (friend) {
                 if (friend != null) {
                     res.json(friend);
                 }
@@ -104,9 +104,9 @@ exports.acceptFriend = function (req, res) {
 }
 
 exports.declineFriend = function (req, res) {
-    db.FriendsMap.update({ status: "declined" }, { where: { iduser:req.body.idfriend , idfriend: req.params.userId } }).then(function (result) {
+    db.FriendsMap.update({ status: "declined" }, { where: { iduser:req.body.idfriend , idfriend: +req.params.userId } }).then(function (result) {
         if (result == 1) {
-            db.FriendsMap.findOne({ where: { iduser: req.params.userId, idfriend: req.body.idfriend } }).then(function (friend) {
+            db.FriendsMap.findOne({ where: { iduser: +req.params.userId, idfriend: req.body.idfriend } }).then(function (friend) {
                 if (friend != null) {
                     res.json(friend);
                 }
@@ -128,7 +128,7 @@ exports.addFriendsByName = function (req, res) {
     db.User.findOne({ where: { Name: req.body.username } }).then(function (user) {
         if (user != null) {
             console.log("feur2")
-            db.FriendsMap.create({ iduser: req.params.userId, idfriend: user.iduser, date: new Date(), status: "pending" }).then(function (friend) {
+            db.FriendsMap.create({ iduser: +req.params.userId, idfriend: user.iduser, date: new Date(), status: "pending" }).then(function (friend) {
                 if (friend != null) {
                     console.log("feur3")
                     res.json(friend);
